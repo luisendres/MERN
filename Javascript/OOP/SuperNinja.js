2344bef1191fa90e9b13f29fb6869b4488e5e27b
Luis Endres <luisendres@yahoo.com>
2021-08-30 23:15:36 -0700

    initial commit/finished

---
+++ Javascript/OOP/SuperNinja.js
@@ -0,0 +1,50 @@
+// parent Ninja class
+class Ninja {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.speed = 3;
+        this.strengh = 3;
+    }
+
+    sayName() {
+        console.log(this.name);
+    }
+
+    showStats() {
+        console.log("Name:", this.name, "Health:", this.health, "Speed:", this.speed,  "Strength:", this.strengh);
+    }
+    drinkSake() {
+        this.health += 10;
+        console.log(this.health);
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+
+// child Sensei class
+class Sensei extends Ninja {
+    constructor(name, health=200, speed=10, strength=10, wisdom=10) {
+        super(name);
+        this.health = health;
+        this.speed = speed;
+        this.strengh = strength;
+        this.wisdom = wisdom;
+    }
+
+    speakWisdom() {
+        const drinker = super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.sayName();
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
