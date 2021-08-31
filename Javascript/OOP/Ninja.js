0b98c4ea6ae99d6b61e7f6ea988e0af3edf984c7
Luis Endres <luisendres@yahoo.com>
2021-08-30 22:58:47 -0700

    inital commit/finished

---
+++ Javascript/OOP/Ninja.js
@@ -0,0 +1,25 @@
+class Ninja {
+    constructor(name){
+        this.name = name;
+        this.health = 100;
+        this.speed = 3;
+        this.strengh = 3;
+    }
+
+    sayName() {
+        console.log(this.name);
+    }
+
+    showStats() {
+        console.log("   Health:", this.health, "Speed:", this.speed,  "Strength:", this.strengh);
+    }
+    drink() {
+        this.health += 10;
+        console.log(this.health);
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drink();
