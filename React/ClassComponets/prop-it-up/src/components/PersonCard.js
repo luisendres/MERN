3c61f409b5128eaac5697941f6443fee69c69180
luisendres <85528951+luisendres@users.noreply.github.com>
2021-09-02 15:06:12 -0700

    changed class into fuctional component

--- React/ClassComponets/prop-it-up/src/components/PersonCard.js
+++ React/ClassComponets/prop-it-up/src/components/PersonCard.js
@@ -1,27 +1,38 @@
 import React from "react";
 
-class PersonCard extends React.Component {
-    constructor(props) {
-        super(props)
+// class PersonCard extends React.Component {
+//     constructor(props) {
+//         super(props)
 
-        this.state = {
-            age: this.props.age
-        };
-    }
-    changeAge = () => {
-        this.setState({ age: this.state.age + 1});
-    };
+//         this.state = {
+//             age: this.props.age
+//         };
+//     }
+//     changeAge = () => {
+//         this.setState({ age: this.state.age + 1});
+//     };
     
-    render() {
-        return (
-            <div>
-                <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.state.age}</p>
-                <p>Hair Color: {this.props.hairColor}</p>
-                <button onClick={this.changeAge}> Birthday Button for {this.props.firstName} {this.props.lastName}</button>
-            </div>
-        );
-    }
+//     render() {
+//         return (
+//             <div>
+//                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
+//                 <p>Age: {this.state.age}</p>
+//                 <p>Hair Color: {this.props.hairColor}</p>
+//                 <button onClick={this.changeAge}> Birthday Button for {this.props.firstName} {this.props.lastName}</button>
+//             </div>
+//         );
+//     }
+// }
+
+//changed the PersonCard class into a fuctional component
+const PersonCard = props => {
+    return(
+        <div>
+            <h1>{ props.lastName }, { props.firstName }</h1>
+            <p>Age: { props.age }</p>
+            <p>Hair Color: { props.hairColor }</p>
+        </div>
+    );
 }
 
 export default PersonCard;
