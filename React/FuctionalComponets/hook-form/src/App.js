e8d5947a7afa189c210c6fbb7d5657dcdd33b1a6
luisendres <85528951+luisendres@users.noreply.github.com>
2021-09-02 20:56:25 -0700

    initial commit/finished

---
+++ React/FuctionalComponets/hook-form/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import './App.css';
+
+import Form from "./components/Form";
+import Results from "./components/Results";
+
+function App() {
+  const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  })
+  return (
+    <div className="App">
+      <Form inputs={state} setInputs={setState} />
+      <Results data={state} />
+    </div>
+  );
+}
+
+export default App;
