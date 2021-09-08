8557c43ed04d343ade7c1c83be41f99b92718131
luisendres <85528951+luisendres@users.noreply.github.com>
2021-09-08 15:14:30 -0700

    added button and switched layout

--- React/APIs/pokemon-api/src/App.js
+++ React/APIs/pokemon-api/src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
-import Pokemon from './Pokemon';
+// import Pokemon from './Pokemon';
+
+import { useState } from 'react';
 
 function App() {
-  return (
+  const [pokemon, setPokemon] = useState(null);
+  // const [error, setError] = useState(null);
+
+  const handleFetchClick = (e) => {
+    //call api
+    fetch("https://pokeapi.co/api/v2/pokemon/")
+    .then((resp) => {
+      // if (!resp.ok) {
+      //   throw new Error(resp.statusText);
+      // }
+      return resp.json();
+    })
+    .then(respData => {
+      console.log(".then triggerd")
+      setPokemon(respData.results);
+    })
+    // .catch((err) => {
+    //   setError(err);
+    // });
+  };
+  
+  return(
     <div className="App">
       <header className="App-header">
-        <Pokemon />
+      <button onClick={handleFetchClick}>Fetch</button>
+
+      {pokemon && pokemon.map((item, index) => <p key={index}>{item.name}</p>)}
       </header>
     </div>
   );
