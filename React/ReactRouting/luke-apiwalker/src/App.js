b0ffa4f7bf4d680ee500c228609646bbc36f4722
luisendres <85528951+luisendres@users.noreply.github.com>
2021-09-09 16:34:51 -0700

    finished

--- React/ReactRouting/luke-apiwalker/src/App.js
+++ React/ReactRouting/luke-apiwalker/src/App.js
@@ -1,12 +1,59 @@
 import './App.css';
+import React from "react";
+import Form from "./Components/Form";
+import People from "./Components/People";
+import Planet from "./Components/Planet";
+
+import { Route, Switch } from "react-router-dom";
+// import { BrowserRouter,Route, Switch } from "react-router-dom";
+
+//see index.js for how <BrowserRouter> is used.
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
+        {/* <nav class="Nav">
+          <p>Search for:</p>
+          <select name="" id="">
+            <option value="people">people</option>
+            <option value="planets">planets</option>
+          </select>
+          <p>ID:</p>
+          <input type="number" />
+          <button>Search</button>
+        </nav> */}
+        <Form />
+      <Switch>
+        <Route path="/people/:id">
+          <People />
+        </Route>
+        <Route path="/planets/:id">
+          <Planet />
+        </Route>
+      </Switch>
       </header>
     </div>
   );
 }
+
+// export default App;
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <h1>Luke API-walker</h1>
+//       <Form />
+//       <Switch>
+//         <Route path="/people/:id">
+//           <People />
+//         </Route>
+//         <Route path="/planets/:id">
+//           <Planet />
+//         </Route>
+//       </Switch>
+//     </BrowserRouter>
+//   );
+// }
 
 export default App;
---
+++ React/ReactRouting/luke-apiwalker/src/Components/People.js
@@ -0,0 +1,44 @@
+// const People = (props) => {
+//     return <h1>Person</h1>;
+// }
+
+// export default People;
+
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import axios from "axios";
+
+const People = (props) => {
+
+    const [data, setData] = useState({});
+    const { id } = useParams();
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/people/${id}/?format=json`)
+            .then(res => {
+                console.log(res);
+                setData(res.data);
+            })
+            .catch(err => {
+                console.error(err);
+                setData({error: "These are not the droids you are looking for"})
+            });
+    }, [id]);
+
+    return (
+        data.error ?
+        <h1>{data.error}</h1> :
+        <div>
+            <h1>Planet: {data.name}</h1>
+            <p>
+                Height: {data.height}<br/>
+                Mass: {data.mass}<br/>
+                Skin color: {data.skin_color}<br/>
+                Hair Color: {data.hair_color}
+            </p>
+        </div>
+    );
+
+}
+
+export default People;
