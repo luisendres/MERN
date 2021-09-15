5b8f4cd93c7b9e85aa405ec4a6ff8a08fa5eb6d0
luisendres <85528951+luisendres@users.noreply.github.com>
2021-09-15 16:58:57 -0700

    finished with bonus

--- Full Stack MERN/authors-project/client/src/App.js
+++ Full Stack MERN/authors-project/client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import { Route, Switch } from "react-router-dom";
+
+import Authors from "./views/Authors";
+import NewAuthor from "./views/NewAuthor";
+import EditAuthor from "./views/EditAuthor";
+import NotFound from "./views/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Favorite authors</h1>
+      <Switch>
+        <Route exact path="/">
+          <Authors />
+        </Route>
+
+        <Route exact path="/new">
+          <NewAuthor />
+        </Route>
+
+        <Route exact path="/edit/:id">
+          <EditAuthor />
+        </Route>
+
+        <Route component={NotFound} />
+
+      </Switch>
     </div>
   );
 }
--- Full Stack MERN/authors-project/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.listen(8000, () => {
-    console.log("Listening at port 8000");
-})
